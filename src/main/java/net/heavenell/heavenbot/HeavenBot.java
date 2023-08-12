faff71211ea130a8e07ebed872fc3090c58c65d5
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-12 09:01:16 +0200

    comment added

--- src/main/java/net/heavenell/heavenbot/HeavenBot.java
+++ src/main/java/net/heavenell/heavenbot/HeavenBot.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+// comment
 public class HeavenBot implements ModInitializer {
 	public static final String MOD_ID = "heavenbot";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
