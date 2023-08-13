9c4c0eb79bbe55d1c41f173b224862cab393c216
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-13 20:33:15 +0200

    add Method load Responses
    to load all the line in the .json file

--- src/client/java/net/heavenell/heavenbot/gamechat/AutoResponse.java
+++ src/client/java/net/heavenell/heavenbot/gamechat/AutoResponse.java
@@ -1,5 +1,7 @@
 package net.heavenell.heavenbot.gamechat;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 import com.mojang.authlib.GameProfile;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
@@ -8,12 +10,17 @@ import net.minecraft.network.message.SignedMessage;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
 
+import java.io.InputStreamReader;
 import java.time.Instant;
 import java.time.Duration;
 
 public class AutoResponse implements ClientReceiveMessageEvents.Chat{
-    static boolean response = false;
+    private static boolean response = false;
     private Instant lastResponseTime = Instant.MIN;
+
+    private static final String STRING_ONEMOMENT = ",onemoment";
+    private static final String KOYORIOP_PATH = "list/KoyoriOP.json";
+    private static String[] responses;
     @Override
     public void onReceiveChatMessage(Text message, @Nullable SignedMessage signedMessage, @Nullable GameProfile sender, MessageType.Parameters params, Instant receptionTimestamp) {
         String messageString = Text.Serializer.toJson(message).toLowerCase();
@@ -26,6 +33,21 @@ public class AutoResponse implements ClientReceiveMessageEvents.Chat{
                 response = true;
                 lastResponseTime = currentTime;
             }
+        }
+    }
+
+    public static void loadResponses() {
+        try {
+            JsonArray responsesArray = new JsonParser().parse(
+                    new InputStreamReader(AutoResponse.class.getResourceAsStream(KOYORIOP_PATH))
+            ).getAsJsonArray();
+
+            responses = new String[responsesArray.size()];
+            for (int i = 0; i < responsesArray.size(); i++) {
+                responses[i] = responsesArray.get(i).getAsString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
