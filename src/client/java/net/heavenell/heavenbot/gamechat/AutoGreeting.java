4bc1f57ab0a0e5a12fe838299522029a18e1755c
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-14 09:03:42 +0200

    implement onReceiveGameMessage

--- src/client/java/net/heavenell/heavenbot/gamechat/AutoGreeting.java
+++ src/client/java/net/heavenell/heavenbot/gamechat/AutoGreeting.java
@@ -1,4 +1,15 @@
 package net.heavenell.heavenbot.gamechat;
 
-public class AutoGreeting {
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.minecraft.text.Text;
+
+public class AutoGreeting implements ClientReceiveMessageEvents.Game{
+    private static boolean response = false;
+    @Override
+    public void onReceiveGameMessage(Text message, boolean overlay) {
+        String messageString = Text.Serializer.toJson(message).toLowerCase();
+        if (messageString.contains("joined the game")) {
+            response = true;
+        }
+    }
 }
