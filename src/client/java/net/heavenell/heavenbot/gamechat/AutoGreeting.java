bd5794da6085ba7d9d5a4d5a8c21d0371a0d5579
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-14 09:04:15 +0200

    implement sendchat

--- src/client/java/net/heavenell/heavenbot/gamechat/AutoGreeting.java
+++ src/client/java/net/heavenell/heavenbot/gamechat/AutoGreeting.java
@@ -1,5 +1,6 @@
 package net.heavenell.heavenbot.gamechat;
 
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
 import net.minecraft.text.Text;
 
@@ -11,5 +12,14 @@ public class AutoGreeting implements ClientReceiveMessageEvents.Game{
         if (messageString.contains("joined the game")) {
             response = true;
         }
+    }
+
+    public static void sendchat() {
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (response) {
+                client.player.networkHandler.sendChatCommand("Hi");
+                response = false;
+            }
+        });
     }
 }
