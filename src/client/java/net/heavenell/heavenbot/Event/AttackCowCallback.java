58a144803fd32d2033503ff86de7833dbc994c20
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-19 00:44:07 +0200

    update version 0.0.5

--- src/client/java/net/heavenell/heavenbot/Event/AttackCowCallback.java
+++ src/client/java/net/heavenell/heavenbot/Event/AttackCowCallback.java
@@ -2,9 +2,12 @@ package net.heavenell.heavenbot.Event;
 
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.passive.SheepEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
@@ -16,6 +19,20 @@ public class AttackCowCallback implements AttackEntityCallback {
     static boolean SheepHitBool = false;
     @Override
     public ActionResult interact(PlayerEntity player, World world, Hand hand, Entity entity, @Nullable EntityHitResult hitResult) {
+//        MinecraftClient minecraftClient = MinecraftClient.getInstance();
+//        ClientWorld clientWorld = minecraftClient.world;
+//
+//        RegistryKey<World> dimensionKey = MinecraftClient.getInstance().world.getRegistryKey();
+//
+//        if(dimensionKey == World.END){
+//            player.sendMessage(Text.literal("END"));
+//        }
+//        else if(dimensionKey == World.NETHER){
+//            player.sendMessage(Text.literal("NETHER"));
+//        }
+//        else if (dimensionKey == World.OVERWORLD) {
+//            player.sendMessage(Text.literal("OVERWORLD"));
+//        }
         if (entity instanceof SheepEntity) {
             SheepHitBool = true;
             player.sendMessage(Text.literal("You just hit a Cow!"));
--- src/client/java/net/heavenell/heavenbot/gamechat/AutoFarewell.java
+++ src/client/java/net/heavenell/heavenbot/gamechat/AutoFarewell.java
@@ -8,19 +8,28 @@ import net.minecraft.network.message.SignedMessage;
 import net.minecraft.text.Text;
 import org.jetbrains.annotations.Nullable;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Random;
 
 public class AutoFarewell implements ClientReceiveMessageEvents.Chat{
     private static boolean response = false;
+    private Instant lastFarewellTime = Instant.MIN;
     private static String farewell = "matanene!";
 
     @Override
     public void onReceiveChatMessage(Text message, @Nullable SignedMessage signedMessage, @Nullable GameProfile sender, MessageType.Parameters params, Instant receptionTimestamp) {
         String messageString = Text.Serializer.toJson(message).toLowerCase();
         if ((messageString.contains("matanene") || messageString.contains("abayo")) && !sender.getName().equals("Heavenell")) {
-            sendchat();
-            response = true;
+            Duration cooldownDuration = Duration.ofSeconds(0);
+            Instant currentTime = Instant.now();
+            Duration timeSinceLastFarewell = Duration.between(lastFarewellTime, currentTime);
+
+            if (timeSinceLastFarewell.compareTo(cooldownDuration) >= 0) {
+                response = true;
+                lastFarewellTime = currentTime;
+                sendchat();
+            }
         }
     }
 
