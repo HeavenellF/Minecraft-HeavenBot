744803dd7db1aa7623d11d9ef774d341564c9fd6
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-19 01:42:00 +0200

    testing gamemode change

--- src/client/java/net/heavenell/heavenbot/Event/KeyInputHandler.java
+++ src/client/java/net/heavenell/heavenbot/Event/KeyInputHandler.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.world.GameMode;
 import org.lwjgl.glfw.GLFW;
 
 public class KeyInputHandler {
@@ -17,6 +18,7 @@ public class KeyInputHandler {
             if(drinkingKey.wasPressed()) {
                 // This happens when our custom key is pressed
                 client.player.networkHandler.sendChatMessage("/roll");
+                client.interactionManager.setGameMode(GameMode.CREATIVE);
             }
         });
     }
