8b87e8ba78facc85dab5bf37bb1f1924609651ff
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-18 23:25:08 +0200

    add Condition

--- src/client/java/net/heavenell/heavenbot/gamechat/AutoFarewell.java
+++ src/client/java/net/heavenell/heavenbot/gamechat/AutoFarewell.java
@@ -1,4 +1,20 @@
 package net.heavenell.heavenbot.gamechat;
 
-public class AutoFarewell {
+import com.mojang.authlib.GameProfile;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.network.message.SignedMessage;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Instant;
+
+public class AutoFarewell implements ClientReceiveMessageEvents.Chat{
+
+
+    @Override
+    public void onReceiveChatMessage(Text message, @Nullable SignedMessage signedMessage, @Nullable GameProfile sender, MessageType.Parameters params, Instant receptionTimestamp) {
+        String messageString = Text.Serializer.toJson(message).toLowerCase();
+        if ((messageString.contains("matanene") || messageString.contains("abayo")) && !sender.getName().equals("Heavenell")) {
+    }
 }
