9de342e6d924ce493ee3ad256f185e28409897c8
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-26 00:53:03 +0200

    Update ReviveOnCall.java

--- src/client/java/net/heavenell/heavenbot/gamechat/ReviveOnCall.java
+++ src/client/java/net/heavenell/heavenbot/gamechat/ReviveOnCall.java
@@ -17,7 +17,7 @@ public class ReviveOnCall implements ClientReceiveMessageEvents.Chat{
     public void onReceiveChatMessage(Text message, @Nullable SignedMessage signedMessage, @Nullable GameProfile sender, MessageType.Parameters params, Instant receptionTimestamp) {
         String messageString = Text.Serializer.toJson(message).toLowerCase();
         MinecraftClient mc = MinecraftClient.getInstance();
-        if (messageString.contains("heaven.respawn") {
+        if (messageString.contains("heaven.respawn")) {
             mc.player.networkHandler.sendPacket((Packet<?>)new ClientStatusC2SPacket(ClientStatusC2SPacket.Mode.PERFORM_RESPAWN));
         }
     }
