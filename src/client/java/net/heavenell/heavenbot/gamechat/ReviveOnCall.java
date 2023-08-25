41d766b8ed040d38ce7b3427476a135cfe8c23d2
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-26 00:52:18 +0200

    Update ReviveOnCall.java

--- src/client/java/net/heavenell/heavenbot/gamechat/ReviveOnCall.java
+++ src/client/java/net/heavenell/heavenbot/gamechat/ReviveOnCall.java
@@ -1,4 +1,24 @@
 package net.heavenell.heavenbot.gamechat;
 
-public class ReviveOnCall implements {
+import com.mojang.authlib.GameProfile;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.network.message.MessageType;
+import net.minecraft.network.message.SignedMessage;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.ClientStatusC2SPacket;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+
+import java.time.Instant;
+
+public class ReviveOnCall implements ClientReceiveMessageEvents.Chat{
+    @Override
+    public void onReceiveChatMessage(Text message, @Nullable SignedMessage signedMessage, @Nullable GameProfile sender, MessageType.Parameters params, Instant receptionTimestamp) {
+        String messageString = Text.Serializer.toJson(message).toLowerCase();
+        MinecraftClient mc = MinecraftClient.getInstance();
+        if (messageString.contains("heaven.respawn") {
+            mc.player.networkHandler.sendPacket((Packet<?>)new ClientStatusC2SPacket(ClientStatusC2SPacket.Mode.PERFORM_RESPAWN));
+        }
+    }
 }
