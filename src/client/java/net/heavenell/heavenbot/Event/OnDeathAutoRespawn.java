3e10eea06bed52e94e1a4ac7fa6d9eaa665aa254
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-08-28 21:58:25 +0200

    no Fall Damage added here

--- src/client/java/net/heavenell/heavenbot/Event/OnDeathAutoRespawn.java
+++ src/client/java/net/heavenell/heavenbot/Event/OnDeathAutoRespawn.java
@@ -8,8 +8,13 @@ import net.minecraft.util.ActionResult;
 public class OnDeathAutoRespawn {
     public static ActionResult onDeath() {
         MinecraftClient mc = MinecraftClient.getInstance();
-        if ( mc.player != null && mc.player.isDead()) {
-            mc.player.networkHandler.sendPacket(new ClientStatusC2SPacket(ClientStatusC2SPacket.Mode.PERFORM_RESPAWN));
+        if (mc.player != null) {
+            if (mc.player.isDead()) {
+                mc.player.networkHandler.sendPacket(new ClientStatusC2SPacket(ClientStatusC2SPacket.Mode.PERFORM_RESPAWN));
+            }
+            if (mc.player.fallDistance >= 2.0f) {
+                mc.player.fallDistance = 0.0f;
+            }
         }
         return ActionResult.PASS;
     }
