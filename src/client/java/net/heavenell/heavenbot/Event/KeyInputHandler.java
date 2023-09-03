c202f9da6402dffd9d0b665a1ba0986aa7815bfd
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-04 00:33:32 +0200

    Update KeyInputHandler.java

--- src/client/java/net/heavenell/heavenbot/Event/KeyInputHandler.java
+++ src/client/java/net/heavenell/heavenbot/Event/KeyInputHandler.java
@@ -4,6 +4,7 @@ import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.GameMenuScreen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -39,14 +40,15 @@ public class KeyInputHandler{
     public static KeyBinding survivalKey;
 
     public static void registerKeyInputs() {
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+        ClientTickEvents.START_CLIENT_TICK.register(client -> {
             if(spectatorKey.wasPressed()) {
                 // This happens when our custom key is pressed
-//                client.player.networkHandler.sendChatMessage("/roll");
+                client.player.networkHandler.sendChatMessage("/roll");
 //                mc.interactionManager.setGameModes(GameMode.SURVIVAL, GameMode.CREATIVE);
 //                client.interactionManager.setGameMode(GameMode.SURVIVAL);
-//                client.player.getAbilities().allowFlying = false;
-                client.setScreen(new TitleScreen(true));
+                client.player.getAbilities().allowFlying = false;
+//                client.setScreen(new GameMenuScreen(true));
+//                client.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.LookAndOnGround(100.0f,100.0f,0.0f,false));
             }
             else if (survivalKey.wasPressed()) {
 //                PlayerAbilities playerAbilities = client.player.getAbilities();
@@ -54,11 +56,12 @@ public class KeyInputHandler{
 //                mc.interactionManager.setGameModes(GameMode.CREATIVE, GameMode.SURVIVAL);
 //                client.interactionManager.setGameMode(GameMode.CREATIVE);
 //                client.player.getAbilities().flying = true;
-//                client.player.getAbilities().allowFlying = true;
+                client.player.getAbilities().allowFlying = true;
 //                client.player.getAbilities().invulnerable = true;
 //                MinecraftClient.getInstance().player.networkHandler.sendPacket(new UpdatePlayerAbilitiesC2SPacket(MinecraftClient.getInstance().player.getAbilities()));
 //                MinecraftClient.getInstance().player.networkHandler.sendPacket(new PlayerAbilitiesS2CPacket(MinecraftClient.getInstance().player.getAbilities()));
 //                client.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.Full(45d,45d, 45d, 45.0f,45.0f, true));
+//                client.setScreen(null);
             }
         });
     }
