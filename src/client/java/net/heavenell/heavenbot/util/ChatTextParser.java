a3a11b9c44d57a15484e7bf7f935ecf5680a0326
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-04 00:32:29 +0200

    can only parse chat from a certain Minecraft Server

--- src/client/java/net/heavenell/heavenbot/util/ChatTextParser.java
+++ src/client/java/net/heavenell/heavenbot/util/ChatTextParser.java
@@ -13,11 +13,23 @@ public class ChatTextParser {
             JsonObject jsonObject = jsonElement.getAsJsonObject();
             if (jsonObject.has("with") && jsonObject.get("with").isJsonArray()) {
                 JsonArray withArray = jsonObject.getAsJsonArray("with");
+
                 if (!withArray.isEmpty()) {
                     JsonElement lastElement = withArray.get(withArray.size() - 1);
-                    if (lastElement.isJsonObject() && lastElement.getAsJsonObject().has("text")) {
-                        String textValue = lastElement.getAsJsonObject().get("text").getAsString();
-                        return textValue;
+                    if (lastElement.isJsonObject() && lastElement.getAsJsonObject().has("extra")
+                            && lastElement.getAsJsonObject().get("extra").isJsonArray()) {
+                        JsonArray extraArray = lastElement.getAsJsonObject().getAsJsonArray("extra");
+
+                        StringBuilder textBuilder = new StringBuilder(); // Initialize a StringBuilder to concatenate the "text" values
+
+                        for (JsonElement extraElement : extraArray) {
+                            if (extraElement.isJsonObject() && extraElement.getAsJsonObject().has("text")) {
+                                String textValue = extraElement.getAsJsonObject().get("text").getAsString();
+                                textBuilder.append(textValue); // Append the "text" value to the StringBuilder
+                            }
+                        }
+
+                        return textBuilder.toString(); // Convert the StringBuilder to a String and return
                     }
                 }
             }
