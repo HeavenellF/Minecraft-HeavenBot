5dcab30faf8fe1d4d22f6c35d9627dab0e73eb4c
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-09-03 21:51:27 +0200

    Create ChatTextParser.java

---
+++ src/client/java/net/heavenell/heavenbot/util/ChatTextParser.java
@@ -0,0 +1,27 @@
+package net.heavenell.heavenbot.util;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class ChatTextParser {
+    public static String parseText(String messageString) {
+        JsonElement jsonElement = JsonParser.parseString(messageString);
+
+        if (jsonElement.isJsonObject()) {
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            if (jsonObject.has("with") && jsonObject.get("with").isJsonArray()) {
+                JsonArray withArray = jsonObject.getAsJsonArray("with");
+                if (!withArray.isEmpty()) {
+                    JsonElement lastElement = withArray.get(withArray.size() - 1);
+                    if (lastElement.isJsonObject() && lastElement.getAsJsonObject().has("text")) {
+                        String textValue = lastElement.getAsJsonObject().get("text").getAsString();
+                        return textValue;
+                    }
+                }
+            }
+        }
+        return "";
+    }
+}
