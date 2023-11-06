233b80ca21f1c974becf9af6d3c10b063ec37150
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-11-07 00:22:46 +0100

    Create AutoRoll.java

---
+++ src/client/java/net/heavenell/heavenbot/gamechat/AutoRoll.java
@@ -0,0 +1,24 @@
+package net.heavenell.heavenbot.gamechat;
+
+import net.heavenell.heavenbot.Event.KeyInputHandler;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.ActionResult;
+
+public class AutoRoll {
+    private static int tickCounter = 0;
+    public static ActionResult roll() {
+        MinecraftClient mc = MinecraftClient.getInstance();
+        tickCounter++;
+        if (mc.player != null && KeyInputHandler.AutoRoll) {
+            if (tickCounter >= 20) {
+                // 620 ticks ≈ 31 seconds
+                tickCounter = 0;
+                sendRollCommandToChat();
+            }
+        }
+        return ActionResult.PASS;
+    }
+    private static void sendRollCommandToChat() {
+        MinecraftClient.getInstance().player.networkHandler.sendChatCommand("roll");
+    }
+}
