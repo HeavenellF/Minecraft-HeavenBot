9b9b8c21801fcfd88bdee65af4974533123b5b42
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-11-20 23:08:23 +0100

    Create HeavenBot.java

---
+++ remappedSrc/net/heavenell/heavenbot/HeavenBot.java
@@ -0,0 +1,17 @@
+package net.heavenell.heavenbot;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+// comment
+public class HeavenBot implements ModInitializer {
+	public static final String MOD_ID = "heavenbot";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
