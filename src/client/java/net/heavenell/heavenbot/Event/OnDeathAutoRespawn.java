bcbe19187beb0428fe9c7e82bb3965d86c5bf7b1
Heavenell <Fauzirahmadiariateja@gmail.com>
2023-11-20 23:09:02 +0100

    Update OnDeathAutoRespawn.java

--- src/client/java/net/heavenell/heavenbot/Event/OnDeathAutoRespawn.java
+++ src/client/java/net/heavenell/heavenbot/Event/OnDeathAutoRespawn.java
@@ -1,9 +1,7 @@
 package net.heavenell.heavenbot.Event;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.player.PlayerAbilities;
 import net.minecraft.network.packet.c2s.play.ClientStatusC2SPacket;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.util.ActionResult;
 
 
@@ -14,14 +12,14 @@ public class OnDeathAutoRespawn {
             if (mc.player.isDead()) {
                 mc.player.networkHandler.sendPacket(new ClientStatusC2SPacket(ClientStatusC2SPacket.Mode.PERFORM_RESPAWN));
             }
-            if (KeyInputHandler.NoFallDamage == true) {
-                if (mc.player.isSneaking()) {
-                    mc.player.getAbilities().setFlySpeed(0.35f);
-                } else mc.player.getAbilities().setFlySpeed(1f);
-                mc.player.fallDistance = 0.0f;
-            }
-            else mc.player.getAbilities().setFlySpeed(0.05f);
-            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
+//            if (KeyInputHandler.NoFallDamage == true) {
+//                if (mc.player.isSneaking()) {
+//                    mc.player.getAbilities().setFlySpeed(0.35f);
+//                } else mc.player.getAbilities().setFlySpeed(1f);
+//                mc.player.fallDistance = 0.0f;
+//            }
+//            else mc.player.getAbilities().setFlySpeed(0.05f);
+//            mc.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
         }
         return ActionResult.PASS;
     }
