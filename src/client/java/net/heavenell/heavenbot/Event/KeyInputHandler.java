a505986bfce7c4a3e7794ed9283ebc108ff3f874
Heavenell <Fauzirahmadiariateja@gmail.com>
2024-03-19 17:46:20 +0100

    Update KeyInputHandler.java

--- src/client/java/net/heavenell/heavenbot/Event/KeyInputHandler.java
+++ src/client/java/net/heavenell/heavenbot/Event/KeyInputHandler.java
@@ -1,51 +1,27 @@
 package net.heavenell.heavenbot.Event;
 
-import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.GameMenuScreen;
-import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.command.argument.EntityArgumentType;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.player.PlayerAbilities;
-import net.minecraft.network.packet.Packet;
-import net.minecraft.network.packet.c2s.play.PlayerInputC2SPacket;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.network.packet.c2s.play.UpdatePlayerAbilitiesC2SPacket;
-import net.minecraft.network.packet.s2c.play.PlayerAbilitiesS2CPacket;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.command.GameModeCommand;
-import net.minecraft.server.command.ServerCommandSource;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Hand;
-import net.minecraft.world.GameMode;
 import org.lwjgl.glfw.GLFW;
-import org.spongepowered.asm.mixin.injection.Inject;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 
 public class KeyInputHandler{
     public static final String KEY_CATEGORY_TUTORIAL = "HeavenBot";
-    public static final String KEY_DRINK_WATER = "key.tutorialmod.drink_water";
-    public static final String KEY_DRINK_WATER2 = "key.tutorialmod.drink_water2";
+    public static final String KEY_TOGGLE_AUTOROLL = "Toggle Autoroll Key";
+    public static final String KEY_FAKE_ROLL = "Fake Roll Key";
 
 
-    public static KeyBinding spectatorKey;
-    public static KeyBinding survivalKey;
+    public static KeyBinding fakerollKey;
+    public static KeyBinding autorollKey;
     public static boolean AutoRoll = false;
 
     public static boolean FlyHack = false;
 
     public static void registerKeyInputs() {
         ClientTickEvents.START_CLIENT_TICK.register(client -> {
-            if(spectatorKey.wasPressed()) {
+            if(fakerollKey.wasPressed()) {
                 // This happens when our custom key is pressed
                 client.player.networkHandler.sendChatMessage("/roll");
 //                if (FlyHack == false){
@@ -63,7 +39,7 @@ public class KeyInputHandler{
 //                client.player.fallDistance = 60.0f;
 //                client.player.networkHandler.sendPacket(new PlayerMoveC2SPacket.OnGroundOnly(true));
             }
-            else if (survivalKey.wasPressed()) {
+            else if (autorollKey.wasPressed()) {
 //                PlayerAbilities playerAbilities = client.player.getAbilities();
 //                playerAbilities.setFlySpeed(1f);
 //                mc.interactionManager.setGameModes(GameMode.CREATIVE, GameMode.SURVIVAL);
@@ -88,14 +64,14 @@ public class KeyInputHandler{
         });
     }
     public static void register() {
-        spectatorKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                KEY_DRINK_WATER,
+        fakerollKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_TOGGLE_AUTOROLL,
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_O,
                 KEY_CATEGORY_TUTORIAL
         ));
-        survivalKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-                KEY_DRINK_WATER2,
+        autorollKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                KEY_FAKE_ROLL,
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_I,
                 KEY_CATEGORY_TUTORIAL
